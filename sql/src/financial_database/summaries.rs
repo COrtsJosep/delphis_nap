8c8fbd8eadcec3552d81ea2fc16b797026175e29
Josep <corts.josep@gmail.com>
2026-04-10 19:29:32 +0200

    added methods to calculate current fund stand

--- sql/src/financial_database/summaries.rs
+++ sql/src/financial_database/summaries.rs
@@ -5,6 +5,14 @@ use sqlx::Connection;
 use std::fmt::Display;
 use strum_macros::{EnumIter, EnumString};
 
+struct CurrentFundStandSummary {
+    name: String,
+    country: String,
+    currency: String,
+    account_type: String,
+    current_value: f64,
+}
+
 #[derive(Debug, Hash, PartialEq, Eq, EnumIter, Clone, EnumString)]
 pub(crate) enum TimeUnit {
     Day,
@@ -57,7 +65,7 @@ impl FinancialDataBase {
         let currency_to_string: String = currency_to.to_string();
 
         let record = sqlx::query_file!(
-            "src/queries/views/calculate_total_income.sql",
+            "src/queries/summaries/calculate_total_income.sql",
             date_from_string,
             date_to_string,
             currency_to_string,
@@ -68,13 +76,44 @@ impl FinancialDataBase {
         Ok(record.total_income.unwrap_or(0.0f64))
     }
 
-    pub(crate) async fn current_fund_stand(&mut self, currency_to: Option<&Currency>) -> () {
+    pub(crate) async fn current_fund_stand(
+        &mut self,
+        currency_to: Option<&Currency>,
+    ) -> Result<Vec<CurrentFundStandSummary>, sqlx::Error> {
         if let Some(currency_to) = currency_to {
-            let currency_to_string: String = currency_to.to_string();
+            match currency_to {
+                Currency::EUR => {
+                    sqlx::query_file_as!(
+                        CurrentFundStandSummary,
+                        "src/queries/summaries/summary_current_fund_stand_eur.sql"
+                    )
+                    .fetch_all(&mut self.connection)
+                    .await
+                }
+                Currency::CHF => {
+                    sqlx::query_file_as!(
+                        CurrentFundStandSummary,
+                        "src/queries/summaries/summary_current_fund_stand_chf.sql"
+                    )
+                    .fetch_all(&mut self.connection)
+                    .await
+                }
+                Currency::SEK => {
+                    sqlx::query_file_as!(
+                        CurrentFundStandSummary,
+                        "src/queries/summaries/summary_current_fund_stand_sek.sql"
+                    )
+                    .fetch_all(&mut self.connection)
+                    .await
+                }
+            }
         } else {
-            sqlx::query_file!("src/queries/views/view_current_fund_stand_nocurrency.sql")
-                .fetch_all(&mut self.connection)
-                .await
+            sqlx::query_file_as!(
+                CurrentFundStandSummary,
+                "src/queries/summaries/summary_current_fund_stand_nocurrency.sql"
+            )
+            .fetch_all(&mut self.connection)
+            .await
         }
     }
 }
